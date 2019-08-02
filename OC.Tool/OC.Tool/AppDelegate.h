14fcef8230331803639928b80e64700a0839e2c3
xueran <873160812@qq.com>
2019-08-02 16:17:23 +0800

    初始化项目

---
+++ OC.Tool/OC.Tool/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OC.Tool
+//
+//  Created by xueranit on 2019/8/2.
+//  Copyright © 2019 xr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OC.Tool/OC.Tool/ViewController.m
@@ -0,0 +1,24 @@
+//
+//  ViewController.m
+//  OC.Tool
+//
+//  Created by xueranit on 2019/8/2.
+//  Copyright © 2019 xr. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    self.view.backgroundColor = UIColor.redColor;
+}
+
+
+@end
